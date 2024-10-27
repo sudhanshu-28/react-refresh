220c81d6122e94d6d0a694de096619948da641f5
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-10-28 00:02:54 +0530

    You can inject any JS code inside JSX inside curly {} braces.

--- App.js
+++ App.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 
 const Title = () => <h1 className="head">Title Component</h1>;
 
+const number = 1000;
+
 // React Functional Component - Function that returns JSZ code is Functional Component
 const HeadingComponent = () => (
   <div className="container">
     {/* Component Composition - It is calling component inside Component  */}
-    <Title />
+    {/* <Title /> */}
+
+    <h2>{number}</h2>
+
     <h1 className="heading">Heading Functional Component</h1>
   </div>
 );
