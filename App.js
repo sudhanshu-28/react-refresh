3417112f07d5d7e7c9208ed48c8e06b3b09f54a8
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-10-27 23:55:00 +0530

    Components Composition & Functional Component.

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element are equivalent to DOM elements
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Heading from createElement"
+// React Element
+const jsxHeading = <h1 id="heading">Heading from JSX</h1>;
+
+const Title = () => <h1 className="head">Title Component</h1>;
+
+// React Functional Component - Function that returns JSZ code is Functional Component
+const HeadingComponent = () => (
+  <div className="container">
+    {/* Component Composition - It is calling component inside Component  */}
+    <Title />
+    <h1 className="heading">Heading Functional Component</h1>
+  </div>
 );
 
-console.log("heading123 => ", heading);
-
-// JSX - is not HTML in JS. JSX is HTML like syntax
-const jsxHeading = <h1 id="heading">Heading from JSX</h1>;
-console.log("jsxHeading123 => ", heading);
+// Browser does not understand Components, Babel just transpile all react elements / function components returns JSX to JS Engine understandable code
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading); // render converts obj to HTML
+root.render(<HeadingComponent />); // render converts obj to HTML
