e434a121e5ff252bf16b9ab2b89570b66cd85ba2
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-10-28 00:00:11 +0530

    Using Arrow Function to create Functional Component is industry standards.

--- App.js
+++ App.js
@@ -1,30 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const Title = () => <h1 className="head">Title Component</h1>;
-
-function Title() {
-  return <h1 className="head">Title Component</h1>;
-}
+const Title = () => <h1 className="head">Title Component</h1>;
 
 // React Functional Component - Function that returns JSZ code is Functional Component
-// const HeadingComponent = () => (
-//   <div className="container">
-//     {/* Component Composition - It is calling component inside Component  */}
-//     <Title />
-//     <h1 className="heading">Heading Functional Component</h1>
-//   </div>
-// );
-
-function HeadingComponent() {
-  return (
-    <div className="container">
-      {/* Component Composition - It is calling component inside Component  */}
-      <Title />
-      <h1 className="heading">Heading Functional Component</h1>
-    </div>
-  );
-}
+const HeadingComponent = () => (
+  <div className="container">
+    {/* Component Composition - It is calling component inside Component  */}
+    <Title />
+    <h1 className="heading">Heading Functional Component</h1>
+  </div>
+);
 
 // Browser does not understand Components, Babel just transpile all react elements / function components returns JSX to JS Engine understandable code
 
