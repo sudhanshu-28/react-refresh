51e4f7188bd1ed4ff34516cf14dc081faa72e433
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-10-27 23:57:17 +0530

    Both Arrow & Normal Functions can be used to create Functional Component.

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-const jsxHeading = <h1 id="heading">Heading from JSX</h1>;
+// const Title = () => <h1 className="head">Title Component</h1>;
 
-const Title = () => <h1 className="head">Title Component</h1>;
+function Title() {
+  return <h1 className="head">Title Component</h1>;
+}
 
 // React Functional Component - Function that returns JSZ code is Functional Component
-const HeadingComponent = () => (
-  <div className="container">
-    {/* Component Composition - It is calling component inside Component  */}
-    <Title />
-    <h1 className="heading">Heading Functional Component</h1>
-  </div>
-);
+// const HeadingComponent = () => (
+//   <div className="container">
+//     {/* Component Composition - It is calling component inside Component  */}
+//     <Title />
+//     <h1 className="heading">Heading Functional Component</h1>
+//   </div>
+// );
+
+function HeadingComponent() {
+  return (
+    <div className="container">
+      {/* Component Composition - It is calling component inside Component  */}
+      <Title />
+      <h1 className="heading">Heading Functional Component</h1>
+    </div>
+  );
+}
 
 // Browser does not understand Components, Babel just transpile all react elements / function components returns JSX to JS Engine understandable code
 
